408539c79615dc8d1573e9edd3a2d3a2e526a65e
Batt866 <25LP6030@nest.edu.mn>
2025-11-27 11:10:58 +0800

    5 footer (#14) package.json oorchlogdson pull hiigeerei main ee
    * tsoojiin hiisen footer
    
    * sign in sign up hiisen
    
    * suuliin sign in shu
    * clerk nemegdsen uchraas bugdeeree mainee 1 pull hiicheerei package.json oorchlogdson
    ---------
    
    Co-authored-by: Amar Bodonguud <64552518+ramamaa@users.noreply.github.com>

--- app/components/navigantion/Header.tsx
+++ app/components/navigantion/Header.tsx
@@ -12,15 +12,25 @@ import {
   NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
 import { useIsMobile } from "../../hooks/use-mobile";
-import { Button } from "@/components/ui/button";
+
 import { Input } from "@/components/ui/input";
+
+import {
+  ClerkProvider,
+  SignIn,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export function Header() {
   const isMobile = useIsMobile();
 
   return (
-    <header className="w-full border-b bg-white/70 backdrop-blur-md sticky top-0 z-50 -ml-20">
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3">
+    <header className="w-full border-b bg-white/70 backdrop-blur-md sticky top-0 z-50 ">
+      <div className="mx-auto flex items-center justify-between px-4 py-3">
         <div className="flex items-center gap-2">
           <Image src="/location.png" alt="logo" width={28} height={28} />
           <h2 className="text-2xl font-semibold text-blue-600">
@@ -92,19 +102,28 @@ export function Header() {
         )}
         <div className="flex gap-3">
           <div className="relative">
-            <IoSearch className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
+            <IoSearch className="absolute left-2 mt-2.5 text-gray-500" />
             <Input
               className="pl-8 w-[150px] bg-white border"
               placeholder="Search..."
             />
           </div>
-
-          <Button className="w-[115px] bg-white text-black font-semibold text-[16px] hover:bg-gray-100">
-            Login
-          </Button>
-          <Button className="bg-blue-600 font-semibold w-[115px] text-[16px] hover:bg-blue-700">
-            Signup
-          </Button>
+          <div className="flex-1"></div>
+          <div className="flex gap-4 items-center">
+            <ClerkProvider>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </ClerkProvider>
+          </div>
         </div>
       </div>
     </header>
