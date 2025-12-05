67726fb6188c0d4f35aff808fb41b8b852693be2
amar <tomreddle70@gmail.com>
2025-12-05 14:56:26 +0800

    Marquee on cards Magic UI

--- app/_components/main/Card.tsx
+++ app/_components/main/Card.tsx
@@ -1,40 +1,33 @@
 import { Badge } from "@/components/ui/badge";
+import { Marquee } from "@/components/ui/marquee";
+
 import { itemType } from "@/lib/types";
 import { MapPin } from "lucide-react";
-// import * from "../../api/items"
-
 import Link from "next/link";
+
 export default async function Card() {
   async function getItems() {
     const response = await fetch("http://localhost:3000/api/items");
-
-    if (!response.ok) {
-      const txt = await response.text();
-      console.error("API ERROR:", txt);
-      throw new Error("API returned non-JSON response");
-    }
-
+    if (!response.ok) throw new Error("Failed to load items");
     return (await response.json()).data;
   }
 
   const items = await getItems();
+  const lostItems = items.filter((i: itemType) => i.type === "lost");
+  const foundItems = items.filter((i: itemType) => i.type === "found");
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mx-auto max-w-5xl gap-5 bg-white mt-16  ">
-      {items
-        .filter((item: itemType) => item.type === "lost")
-        .map((item: itemType) => (
+    <div className="w-5xl mx-auto flex flex-col gap-10 mb-20 overflow-hidden">
+      <Marquee pauseOnHover className="gap-5 [--duration:20s]">
+        {lostItems.map((item: itemType) => (
           <Link key={item._id} href={`/item-detail/${item._id}`}>
-            <div className="border rounded-2xl shadow-md w-60 h-75">
-              <div className="relative h-40 w-60">
-                <Badge
-                  className="absolute flex m-2 flex-end "
-                  variant="destructive"
-                >
-                  {item.type}
+            <div className="border rounded-2xl shadow-md w-60 p-2 bg-white hover:shadow-lg transition">
+              <div className="relative h-40">
+                <Badge variant="destructive" className="absolute m-2">
+                  lost
                 </Badge>
 
-                <Badge className="absolute bottom-2 right-2 whitespace-normal bg-white text-black h-auto leading-tight">
+                <Badge className="absolute bottom-2 right-2 opacity-75 bg-white text-black whitespace-normal h-auto flex gap-1">
                   <MapPin className="w-4 h-4" />
                   {item.location}
                 </Badge>
@@ -42,44 +35,46 @@ export default async function Card() {
                 <img
                   src={item.imageUrl}
                   alt={item.title}
-                  className="w-full h-40 object-cover rounded-t-2xl"
-                ></img>
+                  className="h-40 w-full object-cover rounded-xl"
+                />
               </div>
 
-              <h2 className="text-xl font-bold m-2">{item.title}</h2>
-              <p className="text-gray-600 m-2 mb-10">{item.category}</p>
+              <h2 className="text-lg font-bold mt-2">{item.title}</h2>
+              <p className="text-gray-600">{item.category}</p>
             </div>
           </Link>
-        ))
-        .slice(0, 4)}
-      {items
-        .filter((item: itemType) => item.type === "found")
-        .map((item: itemType) => (
+        ))}
+      </Marquee>
+
+      <Marquee pauseOnHover reverse className="gap-5 [--duration:20s]">
+        {foundItems.map((item: itemType) => (
           <Link key={item._id} href={`/item-detail/${item._id}`}>
-            <div className="border rounded-2xl shadow-md">
-              <div className="relative h-40 w-">
-                <Badge
-                  className="absolute flex m-2 flex-end bg-green-400"
-                  variant="default"
-                >
-                  {item.type}
+            <div className="border rounded-2xl shadow-md w-60 p-2 bg-white hover:shadow-lg transition">
+              <div className="relative h-40">
+                <Badge variant="default" className="absolute m-2 bg-green-500">
+                  found
                 </Badge>
-                <Badge className="absolute bg-white text-black mt-21 mr-2 top-10 right-0">
-                  <MapPin className="w-4 h-4" /> {item.location}
+
+                <Badge className="absolute bottom-2 right-2 whitespace-normal h-auto opacity-75 bg-white text-black flex gap-1">
+                  <MapPin className="w-4 h-4" />
+                  {item.location}
                 </Badge>
+
                 <img
                   src={item.imageUrl}
                   alt={item.title}
-                  className="w-60 h-40 object-cover rounded-t-2xl"
-                ></img>
+                  className="h-40 w-full object-cover rounded-xl"
+                />
               </div>
 
-              <h2 className="text-xl font-bold m-2">{item.title}</h2>
-              <p className="text-gray-600 m-2 mb-10">{item.category}</p>
+              <h2 className="text-lg font-bold mt-2">{item.title}</h2>
+              <p className="text-gray-600">{item.category}</p>
             </div>
           </Link>
-        ))
-        .slice(0, 4)}
+        ))}
+      </Marquee>
+      <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
+      <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
     </div>
   );
 }
